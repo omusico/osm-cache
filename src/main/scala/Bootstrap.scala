ae3e515395804bd3d7fe16f8d138a3105a3e95d1
ponkotuy <web@ponkotuy.com>
2019-05-22 18:17:37 +0900

    Refine View

--- src/main/scala/Bootstrap.scala
+++ src/main/scala/Bootstrap.scala
@@ -1,13 +1,10 @@
-import javax.servlet.ServletContext
-
 import com.ponkotuy.controllers.OSM
+import javax.servlet.ServletContext
 import skinny.micro.LifeCycle
-import scalikejdbc._
 
 class Bootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
     skinny.DBSettings.initialize()
-    sql"create table person (id bigint not null);".update().apply()(AutoSession)
     OSM.mount(context)
   }
 }
